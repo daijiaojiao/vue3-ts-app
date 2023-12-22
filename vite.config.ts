a5cbc5238c0030d9df255d5957470d8a0fd99ada
daijiaojiao <2439877294@qq.com>
2023-12-22 17:30:55 +0800

    feat: antd v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,35 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 // 模块自动引入
-import AutoImport from "unplugin-auto-import/vite";
-// import { fileURLToPath, URL } from "node:url";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
-// import path from "path";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      imports: ["vue", "vue-router", "pinia"],
-      dts: "src/auto-import.d.ts",
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: 'src/auto-import.d.ts',
       eslintrc: {
         enabled: true,
-        filepath: "./.eslintrc-auto-import.json",
+        filepath: './.eslintrc-auto-import.json',
       },
+    }),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+      dts: 'src/components.d.ts',
+      dirs: [],
     }),
   ],
   resolve: {
     alias: {
       // "@": path.resolve(__dirname, "./src"),
-      "@": "/src",
+      '@': '/src',
     },
   },
-});
+})
